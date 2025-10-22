866e3df1f3148d038edde768ad64c0029c562a63
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2025-10-22 12:34:58 +0200

    UseItemInHand (limited)
    Capsule changes colour when in hand. (OPS! needs to be not kinematic)

--- Assets/Scripts/ChangeColour.cs
+++ Assets/Scripts/ChangeColour.cs
@@ -6,21 +6,31 @@ public class ChangeColour : MonoBehaviour
     Renderer capsuleRenderer;
     Material capsuleMat;
 
+    Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         capsuleRenderer = GetComponent<Renderer>();
         capsuleMat = capsuleRenderer.material;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchColour();
+        }
     }
 
     public void SwitchColour()
     {
-        capsuleMat.DOColor(Random.ColorHSV(), 2.0f);
+        if (rb.isKinematic)
+        {
+            capsuleMat.DOColor(Random.ColorHSV(), 2.0f);
+        }
     }
 }
